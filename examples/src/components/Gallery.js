a17c5ccbf28a968d418cdf5b44de62f674c534f2
Martin Saulis <martin@saulis.com>
2019-04-18 14:29:27 +0300

    commit examples

--- examples/src/components/Gallery.js
+++ examples/src/components/Gallery.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { css, StyleSheet } from 'aphrodite/no-important';
+import { css, StyleSheet } from '../../../src/aphrodite-no-important';
 import Lightbox from 'react-images';
 
 class Gallery extends Component {
