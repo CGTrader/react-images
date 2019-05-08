765edc7e87bc29637c42feea411a0fb620b03ce5
Martin Saulis <martin@saulis.com>
2019-05-08 10:32:29 +0300

    Support for legacy inline 360 viewer [ch27184] (#10)

---
+++ src/components/WebRotatable.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import styles from './WebRotatable.css';
+
+class WebRotatable extends React.Component {
+	constructor () {
+		super();
+		this.id = String(Math.floor(Math.random() * Math.floor(123456789123456789)));
+	}
+
+	componentDidMount () {
+		const rotator = window.WR360.ImageRotator.Create(this.id);
+		rotator.licenseCode = this.props.external_id;
+		rotator.settings.disableRelativeAssets = true;
+		rotator.settings.configFileURL = this.props.file_url;
+		rotator.settings.googleEventTracking = false;
+		rotator.settings.responsiveBaseWidth = 0;
+		rotator.settings.responsiveMinHeight = 0;
+		rotator.runImageRotator();
+	}
+
+	render () {
+		return (
+			<div
+				id={this.id}
+				className={this.props.inline ? styles.containerInline : styles.containerModal}
+			/>
+		);
+	}
+}
+
+export default WebRotatable;
